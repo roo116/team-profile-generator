2f86db8619072d1b03ff0bb3fdbd1933815c23f5
roo116 <roo116@nc.rr.com>
2022-06-18 23:16:39 -0400

    have first set of questions being asked

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+// import classes
+
+const { Console } = require("console")
 const fs = require("fs")
 const inquirer = require("inquirer")
-const generateHtml= require('./utils/generateHtml')
+const Employee = require('./lib/Employee')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
+const Manager = require('./lib/Manager')
+const generateHtml = require('./utils/generateHtml')
 // const { resolve } = require("path")
 
 const basicQs = [
@@ -10,7 +17,7 @@ const basicQs = [
         message: "Please input employee's name"
     },
     {
-        type:"input",
+        type: "input",
         name: "id",
         message: "Please assign employee id"
     },
@@ -24,6 +31,7 @@ const basicQs = [
         name: 'role',
         message: 'Please select job description',
         choices: ['Manager', 'Engineer', 'Intern']
+
     }
 ]
 
@@ -31,11 +39,11 @@ const mgrQs = [
     {
         type: "input",
         name: "office",
-        message: "Assing an office number (Required)"
+        message: "What is the office number (Required)"
     }
 ]
 
-const engQs =[
+const engQs = [
     {
         type: "input",
         name: "github",
@@ -45,59 +53,147 @@ const engQs =[
 
 const intQs = [
     {
-        type:"input",
+        type: "input",
         name: "school",
         message: "Enter school name"
     }
 ]
 
-const promptBasic = () => {
-    inquirer
-        .prompt(basicQs)
-        .then((answers) => {
-            console.log(answers)
-            if(answers.role === "Manager") {
-                promptMgr()
-                return;
-            };
-            if(answers.role === "Engineer") {
-                promptEng()
-                return;
-            };
-            if(answers.role === "Intern") {
-                promptInt()
-                return;
-            }
-        })
-        .catch((err) => {
-            if (err) throw err;
-        })
+function getEmployee() {
+    const allEmployeeArr = []
+    this.manager;
+    this.engineer;
+    this.interm;
 
-    // }
-}
 
-const promptMgr = () => {
-    inquirer
-    .prompt(mgrQs)
-    .then((mAnswers) =>{
-        console.log(mAnswers)
-    })
-}
 
-const promptEng = () => {
-    inquirer
-    .prompt(engQs)
-    .then((mAnswers) =>{
-        console.log(mAnswers)
-    })
-}
+    getEmployee.prototype.startMgr = function () {
+        console.log("Input your team information, starting with you as manager.")
+        inquirer
+            .prompt(basicQs)
+            .then((answers) => {
+                console.log(answers)
+            })
 
-const promptInt = () => {
-    inquirer
-    .prompt(intQs)
-    .then((mAnswers) =>{
-        console.log(mAnswers)
-    })
-}
+    }
 
-promptBasic()
+};
+// ).then()
+//     .prompt(mgrQs)
+//     .then((mAnswers) => {
+//         console.log(mAnswers + answers)
+//     })
+
+
+// .then((answers) => {
+//     console.log(answers)
+// this.manager = new Manager(answers);
+// console.log(this.manager)
+// this.manager.push(allEmployeeArr);
+// console.log(allEmployeeArr);
+// }
+// )
+// }
+
+// getEmployee.prototype.getStaff = function () {
+//     inquirer
+//         .prompt(basicQs)
+//         .then((answers) => {
+//             if (answers.role === 'Manager') {
+//                 inquirer
+//                     .prompt(mgrQs)
+//                     .then((answers) => {
+//                         this.manager = new Manager(answers);
+//                         this.manager.push(allEmployeeArr);
+//                     });
+//             }
+//         })
+// };
+
+//         } else if (answers.role === 'Engineer') {
+//             inquirer
+//             .prompt(engQs)
+//             .then((answers) => {
+//                 this.engineer = new Engineer(answers);
+//                 this.engineer.push(allEmployeeArr)
+
+//             })
+//         }
+//         })
+// }
+// const promptBasic = () => {
+//     inquirer
+//         .prompt(basicQs)
+//         .then((answers) => {
+//             allEmployeeArr.push(answers)
+//             console.log(answers)
+//             if (answers.role === "Manager") {
+//                 promptMgr()
+//                 // return;
+//             };
+//             if (answers.role === "Engineer") {
+//                 promptEng()
+//                 // return;
+//             };
+//             if (answers.role === "Intern") {
+//                 promptInt()
+//                 // return;
+//             }
+//         })
+//         .catch((err) => {
+//             if (err) throw err;
+//         })
+
+//     // }
+// }
+
+// const promptMgr = () => {
+//     inquirer
+//         .prompt(mgrQs)
+//         .then((mAnswers) => {
+//             console.log(mAnswers)
+//         })
+// }
+
+// const promptEng = () => {
+//     inquirer
+//         .prompt(engQs)
+//         .then((mAnswers) => {
+//             console.log(mAnswers)
+//         })
+// }
+
+// const promptInt = () => {
+//     inquirer
+//         .prompt(intQs)
+//         .then((mAnswers) => {
+//             console.log(mAnswers)
+//         })
+// }
+
+// const promptMore = () => {
+//     inquirer
+//         .prompt(
+//             {
+//                 type: "confirm",
+//                 name: "continue",
+//                 message: "Would you like to enter another employee? (y,N)",
+//                 default: "No"
+
+
+//             }
+//         ).then((answer) => {
+//             if (answer) {
+//                 promptBasic(); 
+//             }
+//         })
+// }
+
+// promptBasic()
+
+new getEmployee().startMgr()
+
+
+
+
+
---
+++ test.js
@@ -0,0 +1,13 @@
+function getEmployee() {
+    allEmployeeArr = []
+    console.log(allEmployeeArr)
+    this.manager;
+    console.log(this.manager)
+    this.engineer;
+    console.log(this.engineer)
+    this.interm;
+    console.log(this.intern)
+};
+
+getEmployee()
+
